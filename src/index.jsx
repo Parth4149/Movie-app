885a7a207388dbff737f88aa58beab28b4f4acb2
Parth Prajapati <pp41495233@gmail.com>
2023-05-25 17:27:52 +0530

    fix rating size

--- src/index.jsx
+++ src/index.jsx
@@ -10,11 +10,7 @@ import store from "./app/store";
 
 import ToggleColorMode from "./utils/ToggleColorMode";
 // https://redux-toolkit.js.org/tutorials/quick-start#what-youve-learned
-/* The dispatch function is used with the context API OR state management library such as Redux
-to update the state or data of an application. It is used to trigger an action that will update
-the store, which will then trigger a re-render of the component tree with the updated data.
-The dispatch function takes an action object as an argument, which describes the change to be
-made to the state. */
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
